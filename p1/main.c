3a97882af817c7119a00b65fa2bd6ca405172c72
2025020096 <a58865486@gmail.com>
2026-04-20 15:35:04 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n,a,i=0;
+    scanf("%d",&n);
+    do{
+       scanf("%d",&a);
+       if(n!=a)
+           if(n>a)
+               printf("%d>?\n",n);
+           else
+               printf("%d<?\n",n);
+       else
+           printf("%d==?\n",n);
+       
+      i++;
+    }while(i<5);
+    if(i==5)
+        printf("시도횟수는 5회");
+    
+    
+    return 0;
+}
